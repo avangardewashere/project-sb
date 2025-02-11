578b6b27f9086f50a32f9047f7dd0daac901ef44
avangardewashere <avelpanaligan@gmail.com>
2025-02-11 18:32:10 +0800

    add/navlinks

---
+++ components/main/Navlinks/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import style from "./index.module.scss";
+const Navlinks = () => {
+  return (
+    <div className="flex">
+      <div className={style.navItem}>Home</div>
+      <div className={style.navItem}>Products</div>
+      <div className={style.navItem}>Membership</div>
+      <div className={style.navItem}>Faqs</div>
+    </div>
+  );
+};
+
+export default Navlinks;
