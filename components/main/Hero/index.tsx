24ca865395ce832b25b7058a87e2c3c7533d0487
avangardewashere <avelpanaligan@gmail.com>
2025-02-12 12:55:32 +0800

    add/headline

--- components/main/Hero/index.tsx
+++ components/main/Hero/index.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
-
+import React from "react";
+import style from "./index.module.scss";
 const Hero = () => {
   return (
     <div>
-      <span>Hero</span>
+      <div className={style.headline}>
+        <div className={style.title}>OFFICIAL DISTRIBUTOR OF SALVEO BARLEY</div>
+        <div className={style.description}>
+          Salveo Barley Grass is a high-quality premium barley grass grown in
+          pollution free environment. Proven and tested by thousands of users in
+          the Philippines. It's the best and premium barley grass brand you'll
+          ever find!
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
