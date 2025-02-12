9bc47b131369cda1dcf27cfcf48c8ab38d464e99
avangardewashere <avelpanaligan@gmail.com>
2025-02-12 13:10:18 +0800

    add/product-section-headlin

--- components/main/ProductSection/index.tsx
+++ components/main/ProductSection/index.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
-
+import React from "react";
+import style from "./index.module.scss";
 const ProductSection = () => {
   return (
     <div>
-      
+      <div className={style.headline}>
+        <h2>OUR PRODUCTS</h2>
+        <div>Salveo Barley Grass is Certified:</div>
+        <div>Gluten Free. 100% Natural & Organic. FDA Approved. Halal Approved.</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductSection
+export default ProductSection;
