54653000246bc82ec1c30581ce57a70e4d8b07f7
avangardewashere <avelpanaligan@gmail.com>
2025-02-13 19:05:52 +0800

    fix/product-iem

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,16 +1,19 @@
+ 
 import Header from "../../../components/main/Header";
 import CoverImage from "../../../components/ui/coverImage";
-
+ 
 import Hero from "../../../components/main/Hero";
 import Disclaimer from "../../../components/main/disclaimer";
+import ProductSection from "../../../components/main/ProductSection";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <CoverImage />
-      <Hero />
-      <Disclaimer />
+      <Hero /> 
+      <ProductSection/> 
+      <Disclaimer /> 
     </div>
   );
 }
