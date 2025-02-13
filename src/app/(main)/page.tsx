96d0540e2979dfd6788d15ff881009fd7b2cdb2f
avangardewashere <avelpanaligan@gmail.com>
2025-02-13 17:50:19 +0800

    add/disclaimer-styles

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,15 +1,16 @@
- 
 import Header from "../../../components/main/Header";
 import CoverImage from "../../../components/ui/coverImage";
- 
+
 import Hero from "../../../components/main/Hero";
+import Disclaimer from "../../../components/main/disclaimer";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <CoverImage />
-      <Hero /> 
+      <Hero />
+      <Disclaimer />
     </div>
   );
 }
