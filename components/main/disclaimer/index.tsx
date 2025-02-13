ce75217db446934a84f3539d8e626b3e90740eb4
avangardewashere <avelpanaligan@gmail.com>
2025-02-13 17:26:06 +0800

    add/disclaimer

---
+++ components/main/disclaimer/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import style from "./index.module.scss"
+const Disclaimer = () => {
+  return (
+    <div className=''>
+      
+    </div>
+  )
+}
+
+export default Disclaimer
