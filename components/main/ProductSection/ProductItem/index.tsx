1552af4ee7fbfdf4e26ee9763976d1108af3165d
avangardewashere <avelpanaligan@gmail.com>
2025-02-13 19:04:53 +0800

    fix/product-iem

--- components/main/ProductSection/ProductItem/index.tsx
+++ components/main/ProductSection/ProductItem/index.tsx
@@ -3,7 +3,14 @@ import style from "./index.module.scss"
 const ProductItem = () => {
   return (
     <div className={style.container}>
-        
+        {/* Image */}
+        <div className={style.pdetails}>
+           <p>Salveo Barley Grass Powder in Trial Pack (80grams)</p>
+
+           <button>
+            Add to Cart
+           </button>
+        </div>
     </div>
   )
 }
