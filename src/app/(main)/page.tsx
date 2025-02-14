df3073ee72d2156673821c25a16209fb0fdf9052
avangardewashere <avelpanaligan@gmail.com>
2025-02-15 04:03:34 +0800

    add/testimonial-card

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,19 +1,20 @@
- 
 import Header from "../../../components/main/Header";
 import CoverImage from "../../../components/ui/coverImage";
- 
+
 import Hero from "../../../components/main/Hero";
 import Disclaimer from "../../../components/main/disclaimer";
 import ProductSection from "../../../components/main/ProductSection";
+import Testimonial from "../../../components/main/Testimonial";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <CoverImage />
-      <Hero /> 
-      <ProductSection/> 
-      <Disclaimer /> 
+      <Hero />
+      <ProductSection />
+      <Testimonial />
+      <Disclaimer />
     </div>
   );
 }
