b0addad6b573cd096be22f80e1dc3ed12aeae974
avangardewashere <avelpanaligan@gmail.com>
2025-02-14 18:44:30 +0800

    add/image-container

--- components/main/ProductSection/ProductItem/index.tsx
+++ components/main/ProductSection/ProductItem/index.tsx
@@ -1,18 +1,19 @@
-import React from 'react'
-import style from "./index.module.scss"
+import React from "react";
+import style from "./index.module.scss";
+import Image from "next/image";
 const ProductItem = () => {
   return (
     <div className={style.container}>
-        {/* Image */}
-        <div className={style.pdetails}>
-           <p>Salveo Barley Grass Powder in Trial Pack (80grams)</p>
+      <div className={style.imageContainer}>
+        <Image alt="" src="/images/products/trial-ppack.webp" />
+      </div>
+      <div className={style.pdetails}>
+        <p>Salveo Barley Grass Powder in Trial Pack (80grams)</p>
 
-           <button>
-            Add to Cart
-           </button>
-        </div>
+        <button>Add to Cart</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductItem
+export default ProductItem;
