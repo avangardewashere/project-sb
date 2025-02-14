5f6cf9ec8cd361cadc5131fc31bdaf5b9f0519e4
avangardewashere <avelpanaligan@gmail.com>
2025-02-15 04:16:29 +0800

    add/testimonial-photo

--- components/main/Testimonial/card/index.tsx
+++ components/main/Testimonial/card/index.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const TestimonialCard = () => {
   return (
     <div className={style.container}>
-      <Image src="/" alt="" />
+      <Image src="/images/people/test1.webp" alt="" />
       <p className={style.details}></p>
     </div>
   );
