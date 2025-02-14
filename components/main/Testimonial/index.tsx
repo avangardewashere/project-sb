85b832fd6c5d5125ef3f46d8d5e69455df1d8f4b
avangardewashere <avelpanaligan@gmail.com>
2025-02-15 04:04:57 +0800

    add/testimonial-card

--- components/main/Testimonial/index.tsx
+++ components/main/Testimonial/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import TestimonialCard from './card'
-
+import style from "./index.module.scss"
 const Testimonial = () => {
   return (
-    <div>
+    <div className={style.container}>
       <TestimonialCard />
       <TestimonialCard />
       <TestimonialCard />
