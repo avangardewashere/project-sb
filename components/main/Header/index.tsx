37533bb340de2203e6768533a9813a61205c3b18
avangardewashere <avelpanaligan@gmail.com>
2025-02-16 23:24:16 +0800

    add/icons

--- components/main/Header/index.tsx
+++ components/main/Header/index.tsx
@@ -3,16 +3,19 @@ import style from "./index.module.scss";
 import Image from "next/image";
 import Navlinks from "../Navlinks";
 import NotifBar from "./bar";
+import { Search,  ShoppingBagIcon, } from "lucide-react";
 const Header = () => {
   return (
     <div className={style.header}>
-      <NotifBar/>
+      <NotifBar />
       {/* main */}
       <div className={style.main}>
         <Image src={"/images/SB-Logo.webp"} alt="logo" height={60} width={60} />
         <div className={style.actions}>
-          <div>cart</div>
-          <div>search</div>
+          <div>
+            <Search/> 
+            </div>
+          <div><ShoppingBagIcon /></div>
         </div>
       </div>
       {/* nav links */}
